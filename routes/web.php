07ae9b75ce50aa17e58617d288a9b78decbc5757
Aisyah Nabilah Putri <aisyahnabilahputri@gmail.com>
2026-04-27 08:16:30 +0700

    tugas pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 
@@ -8,16 +7,54 @@ Route::get('/', function () {
 });
 
 Route::get('halo', function () {
-	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <i>www.malasngoding.com</i>";
+	return "<h1>Halo, Selamat datang </h1> di tutorial laravel <i>www.malasngoding.com</i>";
 });
 
 Route::get('blog', function () {
     return view('blog');
+
+});
+Route::get('pertemuan1', function () {
+    return view('pertemuan1');
 });
 
-Route::get('pert5', function () {
-    return view('pertemuan 5');
+Route::get('pertemuan2', function () {
+    return view('pertemuan2');
 });
 
-Route::get('/dosen', [DosenController::class, 'index']);
-Route::get('/biodata', [DosenController::class, 'biodata']);
+Route::get('pertemuan3', function () {
+    return view('pertemuan3');
+});
+
+Route::get('template', function () {
+    return view('template');
+});
+
+Route::get('tugaspertemuan3', function () {
+    return view('tugas3');
+});
+
+Route::get('pertemuan4', function () {
+    return view('pertemuan4');
+});
+
+Route::get('pertemuan5index', function () {
+    return view('pertemuan5index');
+});
+
+Route::get('pertemuan5makeoverig', function () {
+    return view('pertemuan5makeoverig');
+});
+
+Route::get('tugaspertemuan5linktree', function () {
+    return view('tugas5linktree');
+});
+
+Route::get('dashboard', function () {
+    return view('menu');
+});
+
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
+
